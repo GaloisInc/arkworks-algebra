0914937cdc39ce31d68bbe93e0066a419502aa9b
James Parker <james@galois.com>
2025-09-08 17:35:59 -0400

    Benches compile

--- curves/bn63/benches/bn63.rs
+++ curves/bn63/benches/bn63.rs
@@ -1,9 +1,9 @@
 use ark_algebra_bench_templates::*;
-use ark_bn254::{fq::Fq, fq2::Fq2, fr::Fr, Bn254, Fq12, G1Projective as G1, G2Projective as G2};
+use ark_bn63::{fq::Fq, fq2::Fq2, fr::Fr, Bn63, Fq12, G1Projective as G1, G2Projective as G2};
 
 bench!(
     Name = "BN254",
-    Pairing = Bn254,
+    Pairing = Bn63,
     G1 = G1,
     G2 = G2,
     ScalarField = Fr,
