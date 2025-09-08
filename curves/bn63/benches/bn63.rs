7a2edd9c11ff4b88ca50be7d6410bab06c7825e6
James Parker <james@galois.com>
2025-09-08 17:36:54 -0400

    Bench name

--- curves/bn63/benches/bn63.rs
+++ curves/bn63/benches/bn63.rs
@@ -2,7 +2,7 @@ use ark_algebra_bench_templates::*;
 use ark_bn63::{fq::Fq, fq2::Fq2, fr::Fr, Bn63, Fq12, G1Projective as G1, G2Projective as G2};
 
 bench!(
-    Name = "BN254",
+    Name = "BN63",
     Pairing = Bn63,
     G1 = G1,
     G2 = G2,
