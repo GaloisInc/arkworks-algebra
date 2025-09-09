e73cd0788db0afa132f97a1e1d0eabce1db563fd
James Parker <james@galois.com>
2025-09-09 16:33:58 -0400

    Revert "Try 128 bit field"
    This reverts commit 9421ba95a9f4862fce0c52d9ed6dccf3fc0ead3e.

--- curves/bn63/src/fields/fr.rs
+++ curves/bn63/src/fields/fr.rs
@@ -1,14 +1,7 @@
-use ark_ff::fields::{Fp64, Fp128, MontBackend, MontConfig};
-
-// #[derive(MontConfig)]
-// #[modulus = "6094503333997212553"]
-// #[generator = "5"]
-// pub struct FrConfig;
-// pub type Fr = Fp64<MontBackend<FrConfig, 1>>;
+use ark_ff::fields::{Fp64, MontBackend, MontConfig};
 
 #[derive(MontConfig)]
-#[modulus = "170141183460469231731687303715884105727"]
-#[generator = "43"]
+#[modulus = "6094503333997212553"]
+#[generator = "5"]
 pub struct FrConfig;
-pub type Fr = Fp128<MontBackend<FrConfig, 2>>;
-
+pub type Fr = Fp64<MontBackend<FrConfig, 1>>;
