9421ba95a9f4862fce0c52d9ed6dccf3fc0ead3e
James Parker <james@galois.com>
2025-09-09 16:13:37 -0400

    Try 128 bit field

--- curves/bn63/src/fields/fr.rs
+++ curves/bn63/src/fields/fr.rs
@@ -1,7 +1,14 @@
-use ark_ff::fields::{Fp64, MontBackend, MontConfig};
+use ark_ff::fields::{Fp64, Fp128, MontBackend, MontConfig};
+
+// #[derive(MontConfig)]
+// #[modulus = "6094503333997212553"]
+// #[generator = "5"]
+// pub struct FrConfig;
+// pub type Fr = Fp64<MontBackend<FrConfig, 1>>;
 
 #[derive(MontConfig)]
-#[modulus = "6094503333997212553"]
-#[generator = "5"]
+#[modulus = "170141183460469231731687303715884105727"]
+#[generator = "43"]
 pub struct FrConfig;
-pub type Fr = Fp64<MontBackend<FrConfig, 1>>;
+pub type Fr = Fp128<MontBackend<FrConfig, 2>>;
+
